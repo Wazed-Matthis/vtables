18d7957d4fba36998a29b442f85db915e0704523
Wazed-Matthis <wazed.wzd@gmail.com>
2022-09-21 19:37:49 +0200

    ok p push

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+use core::fmt::Debug;
+
 pub trait VTable {
     /// # Safety
     ///
     /// This calls function pointers from memory with arbitrary parameters.
     /// This is just about as unsafe as it gets.
     unsafe fn get_virtual<T: Sized>(&self, index: usize) -> T;
+
+    fn new(base: *mut *mut usize) -> Self where Self: Sized;
+
+    fn as_ptr(&self) -> *const usize;
 }
